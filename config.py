9d075a04ce4f573f0d0c73cf42ad794aea1d2e24
adkince <delovoyspetron@gmail.com>
2026-05-13 14:38:10 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+BOT_TOKEN = "8722128569:AAHOpa5-_TkD7ztCTws_8iD2DyalzTNwpV0"
+
+CRYPTOBOT_TOKEN = "CRYPTO_PAY_API_TOKEN"
+
+MARZBAN_URL = "https://twistspeed.duckdns.org:8000/dashboard/#/login"
+MARZBAN_USERNAME = "PolandPanel"
+MARZBAN_PASSWORD = "ThisMac12042009"
+
+ADMIN_ID = 861777713
+
+SUPPORT_USERNAME = "@adkince"
+
+TARIFFS = {
+    "1_month": {
+        "days": 30,
+        "price": 5
+    },
+    "3_month": {
+        "days": 90,
+        "price": 12
+    },
+    "12_month": {
+        "days": 365,
+        "price": 40
+    }
+}
